c1bfcbb6225d2c979d84f1c65df4b80fec3764b6
Austin Hwang <austin.y.hwang@gmail.com>
2018-04-15 23:50:01 -0400

    Add test plan

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# HiveQA
+# Hive Quality Assurance Test Plan
+
+## Objectives
+The objective of these tests is to ensure a smooth join flow for brand new users to increase user retention rates, as well as ensure that there are no major bugs in the registration form. 
+
+## Scope
+These set of tests are limitted to the scope of the registration/sign-in process. Anything after initial sign-up is not included.
+
+## Assumptions
+Tests assume that all pop-ups in the log-in page, registration page, and storage options page (e.g Google, Slack, Dropbox, and Box) have been correctly integrated in that after logging in with each respective account, the process continues smoothly without any bugs. It also assumes that if one of the organization size buttons can be pressed, the other two also works and that if one item can be filled out in the Hive action list and invitation page, then the other two items can also be filled out.
+
+## Test Approach
+We conduct four main tests:
+1. Test that the log-in page loads correctly using staging.hive.com
+2. Test that users can join as a new user if they do not have an account already, but skipping all optional fields. We will test filling out account details (i.e email and password), creating a workspace and naming an organization, skipping the Hive action list, skipping inviting coworkers, and skipping integrating file storage services. Then, we will log-out and attempt logging in with the freshly created log-in credentials. 
+3. Test that users can join as a new user, but fill out all provided fields. Everything conducted the same as above, but all external account services (e.g Google, Slack, Dropbox) will be opened up as a pop-up just to see that they work, and the action list and inviting coworkers will be filled out. Furthermore, in this run, it will test some variations of possible invalid inputs in various fields of the registration process.
+4. Test that users can reset their password.
+
+## Conclusion
+Registration process overall is very smooth and I didn't spot any major bugs. One thing that caught my attention was that when filling out the phone number field in creating a new account, there wasn't any limit to the phone number format and a users can input any string into the field, including letters and symbols. Finally, when writing tests, I had to delay the driver in a couple places because the aninmation between steps of the registration process was too slow and would break if the the page did not completely load. 
--- test/AcceptanceTest.js
+++ test/AcceptanceTest.js
@@ -11,6 +11,7 @@ import ResetPasswordPage from '../src/pageobjects/ResetPasswordPage';
 describe('Acceptance Tests', function () {
   let driverBuilder
   let driver
+  let sameEmail
 
   beforeEach(async function () {
     driverBuilder = new DriverBuilder()
@@ -18,20 +19,23 @@ describe('Acceptance Tests', function () {
     await driverutils.goToHome(driver)
   })
 
+  // Tests that registration page can load up
+
   it('Loads the registration page', async function () {
     const registrationPage = new RegistrationPage(driver)
-    await registrationPage.isLoaded()
+    await registrationPage.isLoaded() 
   })
+
+  // Tests that users can register by filling out as little of the form as possible, only using required fields
 
   it('Registers new user using only minimum, required information', async function () {
     const registrationPage = new RegistrationPage(driver)
-    let email = registrationPage.randomString() + "@domain.com" ;
+    sameEmail = registrationPage.randomString() + "@domain.com" ;
     let password = registrationPage.randomString();
 
-    //First run
     await registrationPage.isLoaded()
     await registrationPage.join()
-    await registrationPage.fillOutAccount(email, password, false)
+    await registrationPage.fillOutAccount(sameEmail, password, false)
     await driver.sleep(2000)
     await registrationPage.fillOutOrganization(false)
     await driver.sleep(2000)
@@ -43,55 +47,68 @@ describe('Acceptance Tests', function () {
     await registrationPage.nextSteps()
     await registrationPage.logOut()
     await registrationPage.isLoaded()
-    await registrationPage.logIn(email, password)
+    await registrationPage.logIn(sameEmail, password)
     await driver.sleep(2000)
     await registrationPage.logOut()
 
-    //Second run
-    let email2 = registrationPage.randomString() + "@domain.com" ;
-    await registrationPage.isLoaded()
-    await registrationPage.join()
-    await registrationPage.fillOutAccount("", password, true)
-    await registrationPage.fillOutAccount(email, password, true)
-    await registrationPage.accountOptions()
-    await registrationPage.fillOutAccount(email2, password, true)
-    await driver.sleep(2000)
-    await registrationPage.fillOutOrganization(true)
-    await driver.sleep(2000)
-    await registrationPage.fillOutList("Test")
-    await driver.sleep(2000)
-    await registrationPage.fillOutInvite("Test@domain.com")
-    await driver.sleep(2000)
-    await registrationPage.storageOptions()
-    await registrationPage.fillOutStorage()
-    await registrationPage.nextSteps()
-    await registrationPage.logOut()
-    await registrationPage.isLoaded()
-    await registrationPage.logInOptions()
-    await registrationPage.logIn(email, password)
-    await driver.sleep(2000)
-    await registrationPage.logOut()
   })
 
-  it('Goes back home at each step', async function () {
+  /* Tests that users can register by filling out all optional fields. Note: I realized that you can put anything in 
+  phone number field (e.g numbers, letters, improper phone number format) and it still works */
+
+  it('Registers new user, using all account options and filling out all available fields', async function () {
+     const registrationPage = new RegistrationPage(driver)
+     let email2 = registrationPage.randomString() + "@domain.com" ;
+     let password = registrationPage.randomString();
+
+     await registrationPage.isLoaded()
+     await registrationPage.join()
+     await registrationPage.fillOutAccount("", password, true) // Tries empty email
+     await registrationPage.fillOutAccount(sameEmail, "123", true) // Tries invalid email and password
+     await registrationPage.accountOptions() // Tests pop-ups for google and slack options
+     await registrationPage.fillOutAccount(email2, password, true) // Tries valid email
+     await driver.sleep(2000)
+     await registrationPage.fillOutOrganization(true)
+     await driver.sleep(2000)
+     await registrationPage.fillOutList("Test")
+     await driver.sleep(2000)
+     await registrationPage.fillOutInvite("Test@domain.com")
+     await driver.sleep(2000)
+     await registrationPage.storageOptions() // Tests pop-ups for google drive, box, and dropbox options
+     await registrationPage.fillOutStorage()
+     await registrationPage.nextSteps()
+     await registrationPage.logOut()
+     await registrationPage.isLoaded()
+     await registrationPage.logInOptions() // Tests login pop-ups for google and slack options
+     await registrationPage.logIn(sameEmail, password) // Tests incorrect log-in
+     await registrationPage.logIn(email2, password) // Correct log-in
+     await driver.sleep(2000)
+     await registrationPage.logOut()
+  })
+
+  // Tests that users can return to sign in page in case they accidentally clicked on join 
+
+  it('Goes back home after a step', async function () {
     const registrationPage = new RegistrationPage(driver)
     await registrationPage.isLoaded()
     await registrationPage.join()
-    await registrationPage.goBack()
+    await registrationPage.goBack() // Tests returning to log-in page after moving forward a step, assumes same for all steps after
     assert.isDefined(await registrationPage.getLogo(), "Expected main page logo")
   })
 
-  it('Forgot password works', async function () {
+  // Tests that users can go through with password reset if they forgot log-in information
+  
+  it('Forgot password functionality', async function () {
     const registrationPage = new RegistrationPage(driver)
     const resetPasswordPage = new ResetPasswordPage(driver)
 
     let email = registrationPage.randomString() + "@domain.com" ;
     await registrationPage.isLoaded()
     await resetPasswordPage.forgotPass()
-    await resetPasswordPage.fillOut("")
+    await resetPasswordPage.fillOut("") // First tries empty email
     const warning = await resetPasswordPage.getWarning()
     assert.strictEqual(warning, 'Email required', 'Warning should match expected message')
-    await resetPasswordPage.fillOut(email)
+    await resetPasswordPage.fillOut(email) // Then tries correct email
     const message = await resetPasswordPage.getSuccessMessage()
     assert.strictEqual(message, 'Thanks, check your email and you should have a password reset link if an account with the associated email address exists.', 'Message should match expected message')
   })
